8ff9ae5570b0f4a6783dcc318dfbef9c88b0f7c7
unuuns <dicksonmuhuha977@gamil.com>
2025-01-24 01:37:17 +0300

    Add Main.java with implementation of user authentication

---
+++ Main.java.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main;
+
+/**
+ * this a program to print hello world
+ *registration number=CT101/G/19497/23
+ * Wambui Dickson  Muhuha
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //this prints out hello world
+        System.out.println("Hello World!");
+    }
+}
