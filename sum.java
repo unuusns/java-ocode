fae4c9d7a83b1a3d2283e2d28c360d949a99e64c
unuuns <dicksonmuhuha977@gamil.com>
2025-01-24 08:38:44 +0300

    Remove Main.java and add sum.java with initial implementation

---
+++ sum.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sum;
+
+/**
+ *this a code to calculate the sum of two products
+ * registration number=CT101/G/19497/23
+ * Wambui DICKSON MUHUHA
+ */
+public class Sum {
+    public static void main(String[] args) {
+
+        // Declare two integer variables and assign values to them.
+        int x = 5; // First number to be added.
+        int y = 7; // Second number to be added.
+
+        // Calculate the sum of x and y 
+        int sum = x + y; // this the result of ading x and y .
+
+        // Print the result
+        System.out.println("The sum of " + x + " and " + y + " is: " + sum);
+
+    }
+}
